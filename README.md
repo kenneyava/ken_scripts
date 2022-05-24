26a231746e145b927126b680f0dc26dac197e84c
Kenneth Aung <kenneth.aung@gmail.com>
2022-05-24 13:42:44 -0400

    added search for no owner or group

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ![Pull down the script using wget](https://medium.com/theloudcloud/download-a-file-from-github-using-linux-commands-f0ce4e154c25)
 
-wget https://raw.githubusercontent.com//kenneyava/ken_scripts/main/<file_name>
+wget https://raw.githubusercontent.com//kenneyava/ken_scripts/main/wrldwriteable.py && chmod u+x wrldwriteable.py
---
+++ noownerorgrp.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import os
+import re
+import pwd
+
+PATTERN = re.compile(r'^/proc|^/var|^/sys|^/dev|^/run|^/tmp')
+
+def walk_dir():
+    for root, dirs, files in os.walk('/', topdown=False):
+        # print("files")
+        for name in files:
+            filename = os.path.join(root, name)
+            if (not (os.path.islink(filename))) and (not PATTERN.findall(root)):  
+                stats = os.stat(filename)
+                if pwd.getpwuid(stats.st_uid).pw_name == '':
+                    print( filename + " " + stats.st_uid)
+
+
+walk_dir()
